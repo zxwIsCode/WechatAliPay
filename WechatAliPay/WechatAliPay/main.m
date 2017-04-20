c3abc061f548f41997ba97d62ff59f1059c0a59c
张小伟 <1824496534@qq.com>
2017-04-20 14:43:31 +0800

    第一次初始化项目

---
+++ WechatAliPay/WechatAliPay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WechatAliPay
+//
+//  Created by 李保东 on 17/4/20.
+//  Copyright © 2017年 DaviD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
